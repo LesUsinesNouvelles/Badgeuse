fec3d4697a5a0771a3443f41f6a5629fcb7b7fb4
Qdeyna <quentin.deyna@gmail.com>
2016-06-27 11:28:18 +0200

    - ‘affichage_inscription.php’ - ‘ajoutBadge.php’ - ‘ajoutBadgeuse.php’ - ‘badgeuse_acces.php’ - ‘connect_db.php’ - ‘entete.php’ - ‘footer.php’ - ‘gestionBadges.php’ - ‘historique_acces.php’ - ‘index.php’ - ‘infosBadge.php’ - ‘inscription.php’ - ‘profile.php’ - ‘updateNumBadge.php’

--- connect_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$bdd = new PDO ('mysql:host=localhost:8889;dbname=Badgeuse_Francois;charset=utf8','root','root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-	if (!$bdd)
-	{	
-		echo '<p id="erreur">La connexion à la base de données à échouée</p>';
-	}
-
-?>
